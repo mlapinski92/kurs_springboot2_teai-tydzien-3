69c2acf3b107bd5feb97adefbc6a5ef33b2dcbd9
mlapinski92 <maciej.lapinski92@gmail.com>
2020-05-05 17:34:50 +0200

    Initial commit

---
+++ src/main/java/pl/lapinski/car_project/Car.java
@@ -0,0 +1,69 @@
+package pl.lapinski.car_project;
+
+public class Car {
+
+    private long id;
+    private String mark;
+    private String model;
+    private String colour;
+
+    public Car(long id, String mark, String model, String colour) {
+        this.id = id;
+        this.mark = mark;
+        this.model = model;
+        this.colour = colour;
+    }
+
+    public Car() {
+    }
+
+    public void modifyCar(Car car) {
+        String newMark = car.getMark();
+        String newModel = car.getModel();
+        String newColor = car.getColour();
+
+        if (newMark != null) {
+            this.mark = newMark;
+        }
+        if (newModel != null) {
+            this.model = newModel;
+        }
+        if (newColor != null) {
+            this.colour = newColor;
+        }
+    }
+
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+}
---
+++ src/test/java/pl/lapinski/car_project/CarProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lapinski.car_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
